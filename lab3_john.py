59b83c7cb5777fc8ca37e8706ea1e775d4377df6
John Paul <ahmadk.price@gmail.com>
2026-06-23 15:49:28 -0700

    lab 3 data structures

---
+++ lab3_john.py
@@ -0,0 +1,15 @@
+john wayne | Lab 3 | Intro to Python
+username: johnwayne len()
+john
+wayne len()
++welcome to loop, @johnwayne!
+username[0] = "X" # run this, it breaks on purpose
+feed = sports, news, entertainment len()
+.append()
+.pop()
+user_profile = {
+    "username": "followers",
+    "verified"}
+profile [0] # run this, it breaks on purpose
+.get("username")
+@johnwayne has 250 followers and 3 posts. Top post: First day at Loop
